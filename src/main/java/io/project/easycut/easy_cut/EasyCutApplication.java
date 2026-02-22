f60f856ddb9391bef4552ed5cfa607952812197a
castle-bird <seongjo0501@gmail.com>
2026-02-22 15:26:39 +0900

    first commit

---
+++ src/main/java/io/project/easycut/easy_cut/EasyCutApplication.java
@@ -0,0 +1,13 @@
+package io.project.easycut.easy_cut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyCutApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EasyCutApplication.class, args);
+  }
+
+}
---
+++ src/test/java/io/project/easycut/easy_cut/EasyCutApplicationTests.java
@@ -0,0 +1,13 @@
+package io.project.easycut.easy_cut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyCutApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
