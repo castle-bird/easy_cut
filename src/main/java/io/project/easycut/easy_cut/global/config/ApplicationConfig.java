ce7ee50ca1a9af9e629637690ab8bcf530667d60
castle-bird <seongjo0501@gmail.com>
2026-02-25 16:45:47 +0900

    추후 스케쥴러 사용을 위한 어노테이션 추가

--- src/main/java/io/project/easycut/easy_cut/global/config/ApplicationConfig.java
+++ src/main/java/io/project/easycut/easy_cut/global/config/ApplicationConfig.java
@@ -2,8 +2,12 @@ package io.project.easycut.easy_cut.global.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
-@EnableJpaAuditing
+@EnableScheduling   // 스케줄링 활성화
+@EnableJpaAuditing  // 생성일/수정일 자동 관리를 위해 필요
 public class ApplicationConfig {
+
+
 }
